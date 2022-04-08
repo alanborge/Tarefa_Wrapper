e051e2384f2a90a7b8b1acea68ceacd2a0fc1650
alanborge <alanborges2020@outlook.com>
2022-04-08 18:05:36 -0300

    inserindo o JAvaDoc

--- src/br/com/Allaborges/Converssao_Tipo_Dados.java
+++ src/br/com/Allaborges/Converssao_Tipo_Dados.java
@@ -1,2 +1,51 @@
-package br.com.Allaborges;public class Converssao_Tipo_Dados {
+package br.com.Allaborges;
+
+/**
+ * classe converte tipo de dados
+ */
+public class Converssao_Tipo_Dados {
+   /**
+    * unboxing
+    */
+   private short tipo_primitivo;
+   private Integer validacao = Integer.valueOf(tipo_primitivo);
+   private Long numI = Long.valueOf(validacao);
+
+   public Long getNumI() {
+      return numI;
+   }
+
+   public void setNumI(Long numI) {
+      this.numI = numI;
+   }
+
+   public short getTipo_primitivo() {
+      return tipo_primitivo;
+   }
+
+   public void setTipo_primitivo(short tipo_primitivo) {
+      this.tipo_primitivo = tipo_primitivo;
+   }
+
+   public Integer getValidacao() {
+      return validacao;
+   }
+
+   public void setValidacao(Integer validacao) {
+      this.validacao = validacao;
+   }
+
+   public void impressaoDiretaConversao(){
+   System.out.println("\n" + "casting dados de primitivo para Wrapper" + "\n");
+      setNumI(1111111254999999999l);
+      getNumI();
+      System.out.println(getNumI());
+      System.out.println("---------Nova Validação------------------");
+      short tipo_primitivo2 = 9999;
+      Long novaConvercao = Long.valueOf(tipo_primitivo2);
+      System.out.println(tipo_primitivo2);
+      System.out.println(novaConvercao);
+
+
+   }
 }
--- src/br/com/Allaborges/Principal.java
+++ src/br/com/Allaborges/Principal.java
@@ -1,2 +1,24 @@
-package br.com.Allaborges;public class Principal {
+package br.com.Allaborges;
+
+import java.util.Scanner;
+
+/**
+ * classe principal
+ */
+public class Principal {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Converssao_Tipo_Dados ctp = new Converssao_Tipo_Dados();
+
+        System.out.println("informe o numero primitivo");
+        ctp.getValidacao();
+        ctp.setValidacao(input.nextInt());
+
+        System.out.println("numero convertido em Wrapper: " + ctp.getValidacao());
+        ctp.impressaoDiretaConversao();
+    }
 }
